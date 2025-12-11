a1ce69e038b7d018aaf761a777a930f6774a5049
Sri-Satya-Naren <srisatyanareng@gmail.com>
2025-12-11 12:45:21 +0530

    Add files via upload

---
+++ Task2ANo3.py
@@ -0,0 +1,5 @@
+import math
+num=int(input("enter a number"))
+print("The Square Root of the number is ",num**0.5)
+print("The logrithm of numberis",math.log(num))
+print("The sin of the number is",math.sin(num))
